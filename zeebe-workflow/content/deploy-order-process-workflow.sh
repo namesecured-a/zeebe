9856ca68613119e2dfd4bd565039e9f75b18ee53
Sergey Psekha <pss@ciklum.com>
2020-01-29 15:39:53 +0200

    implement deploy-order-process-workflow

--- zeebe-workflow/content/deploy-order-process-workflow.sh
+++ zeebe-workflow/content/deploy-order-process-workflow.sh
@@ -1,3 +1,7 @@
-ZEEBE_LEADER_ADDRESS=172.17.0.2:26500
+env=./deploy-session.env
 
-./bin/zbctl --insecure --address '$ZEEBE_LEADER_ADDRESS' deploy ./order-process.bpmn
+ZEEBE_LEADER_ADDRESS=172.17.0.2:26500
+echo $ZEEBE_LEADER_ADDRESS
+echo ./bin/zbctl --insecure --address ''$ZEEBE_LEADER_ADDRESS'' deploy ./order-process.bpmn
+
+unset env
